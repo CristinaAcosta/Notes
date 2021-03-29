ae7353efea00552f23f93cd674c229330cfde67d
Cristina Acosta <cacost5695@yahoo.com>
2021-03-29 09:46:10 -0700

    notesssss

--- Develop/server.js
+++ Develop/server.js
@@ -7,4 +7,13 @@ const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 const app = express();
 var PORT = process.env.PORT || 8080;
 
+// Gives access to the assest folder 
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
+
+// Routes for the API's
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
