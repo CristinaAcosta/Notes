9355b73cc43f1ae4ecd9cb871649da3541a73a59
Cristina Acosta <cacost5695@yahoo.com>
2021-03-29 09:46:41 -0700

    notessssss

--- Develop/server.js
+++ Develop/server.js
@@ -17,3 +17,23 @@ app.use(express.static("public"));
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./db/db.json"));
 });
+
+//post function to add new notes
+//UUID creates a unique ID for each note so then we can delete certain notes 
+//Stringify allows us to read it 
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+    const newNote = req.body;
+    new.id = uuid.v4();
+    notes.push(newNote); 
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+});
+
+//Removes the old notes 
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const noNotes = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noNotes));
+})
+
