ec0632d16f6c74005ba75c8a1dcc5663aa9ab07a
Cristina Acosta <cacost5695@yahoo.com>
2021-03-30 10:07:00 -0700

    notessssssss

--- Develop/server.js
+++ Develop/server.js
@@ -2,41 +2,38 @@ const express = require("express");
 const fs = require("fs");
 const notes = require("./db/db.json");
 const path = require("path");
-//const uuid = require("uuid");
-
-
 const app = express();
+
 
 var PORT = process.env.PORT || 8080;
 
-// Gives access to the assest folder 
+// Gives access to assest folder 
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // Routes for the API's
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 //post function to add new notes
 //UUID creates a unique ID for each note so then we can delete certain notes 
 //Stringify allows us to read it 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const newNote = req.body.toUpperCase;
-    newNote.id = uuid.v4();
+    const newNote = req.body;
     notes.push(newNote); 
     fs.writeFileSync("./db/db.json", JSON.stringify(notes));
     res.json(notes);
 });
 
-//calls the HTML
+//calls the home pg
 app.get("/", function (req, res){
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-//Calls to the notes.html folder 
+//Calls to the notes.html 
 app.get("/notes", function (req,res){
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
